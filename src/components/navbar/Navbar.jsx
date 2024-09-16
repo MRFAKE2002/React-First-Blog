9cd7cbc3915028dfda1bcf1a0068781a4320496d
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-09-16 19:01:19 +0330

    Add our site title using props in Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,28 @@
 import style from "./Navbar.module.css";
 
 function Navbar(params) {
-    return (
-        <div className={style.header}>
-            <ul>
-                <li>لیست مقالات</li>
-                <li>ساخت مقاله جدید</li>
-                <li>درباره ما</li>
-            </ul>
-        </div>
-    )
+  /* 
+      zamani ke bekhaim az js estefade konim va dar html gharar bedim bayad dakhel {} bezarim 
+      dar inja baraye fahmidan meghdare 'params' az 'console.log(params);' estefade kardim va dar khoruji 
+      meghdar params mishe:
+      
+      params = {
+          title: 'روزبه شاپ'
+      }
+  */
+
+  console.log(params);
+
+  return (
+    <div className={style.header}>
+      <h3> {params.title} </h3>
+      <ul>
+        <li>لیست مقالات</li>
+        <li>ساخت مقاله جدید</li>
+        <li>درباره ما</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Navbar;
