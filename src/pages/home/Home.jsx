2004acd52ca19e2337f6faf72c2421abbefd4c8f
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-09-17 01:05:51 +0330

    Add our article component into Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,20 @@
+// css
+import style from "./Home.module.css";
+
+// components
 import Navbar from "../../components/navbar/Navbar";
+import Article from "../../components/article/Article";
 
 function Home(params) {
   return (
-    <div>
+    <div className={style.homeWrapper}>
       <Navbar title="روزبه شاپ" />
+      <div className="container">
+        <h2>مقالات جدید</h2>
+        <div className={style.homeArticles}>
+          <Article />
+        </div>
+      </div>
     </div>
   );
 }
