c9b8f4bd4a9f4f78660a209f222bbcaa5c84c3cb
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-09-16 19:02:08 +0330

    Make our home page as Home.jsx component

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/navbar/Navbar";
+
+function Home(params) {
+  return (
+    <div>
+      <Navbar title="روزبه شاپ" />
+    </div>
+  );
+}
+
+export default Home;
