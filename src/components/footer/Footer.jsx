1e53e53ba76551078e713b5b97a90985a18eb438
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-09-19 00:53:07 +0330

    Make AboutUs component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,10 @@
+// css
 import style from './Footer.module.css';
 
 function Footer(params) {
     return (
         <div className={style.footer}>
-            <h3>footer</h3>
+            <h2>footer</h2>
         </div>
     )
 }
--- src/pages/about-us/AboutUs.jsx
+++ src/pages/about-us/AboutUs.jsx
@@ -1,11 +1,27 @@
+// css
+import style from "./AboutUs.module.css";
 
+// components
+import Navbar from "../../components/navbar/Navbar";
+import Footer from "../../components/footer/Footer";
+
+// Image
+import image from "../../components/assets/images/articleImage.png";
 
 function AboutUs(params) {
-    return (
-        <div>
-            about us
-        </div>
-    )
+  return (
+    <div className={style.aboutUs}>
+      <Navbar title="روزبه شاپ" />
+
+      <div className="container">
+        <h1>درباره ما</h1>
+        <p>این درباره سایت وبلاگی هستش که توسط ریکت ساختم</p>
+        <h3>فعالیت ما در کدان شهرها است؟</h3>
+        <img src={image} alt="" />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
-export default AboutUs
+export default AboutUs;
